6a75072eccebcdb54ff8e2dc8bb947698254f962
danglvh1004 <lvhd.hdlv@gmail.com>
2021-01-26 16:36:54 +0700

    Xử lý đồng bộ

--- untitled1/src/com/company/Bank.java
+++ untitled1/src/com/company/Bank.java
@@ -1,12 +1,13 @@
 package com.company;
 
 import java.util.Arrays;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.*;
 
 public class Bank {
 
     Lock lock;
+
+    private Condition condition;
 
     public double[] accounts;
 
@@ -14,16 +15,27 @@ public class Bank {
         accounts = new double[n];
         Arrays.fill(accounts, initMoney);
         lock = new ReentrantLock();
+        condition = lock.newCondition();
     }
 
     public void transfer(int from, int to, double amount) {
         lock.lock();
         try {
             System.out.print(Thread.currentThread().getName());
+
+            while (accounts[from] < amount) {
+                System.out.printf(" Not enough: %.2f \n", amount);
+                try {
+                    condition.await();
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
             accounts[from] -= amount;
             System.out.printf(" %10.2f form %d to %d", amount, from, to);
             accounts[to] += amount;
             System.out.printf(" Total Balance: %10.2f\n", getTotalBank()); //10 khoảng trắng và 2 số đằng sau
+            condition.signalAll();
         } finally {
             lock.unlock();
         }
