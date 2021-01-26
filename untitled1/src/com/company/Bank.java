5e5f585b1fad3f759bad37e75a8f24aed0e25e14
danglvh1004 <lvhd.hdlv@gmail.com>
2021-01-26 16:41:24 +0700

    Xử lý đồng bộ cách 2

--- untitled1/src/com/company/Bank.java
+++ untitled1/src/com/company/Bank.java
@@ -5,40 +5,28 @@ import java.util.concurrent.locks.*;
 
 public class Bank {
 
-    Lock lock;
-
-    private Condition condition;
-
     public double[] accounts;
 
     Bank(int n, double initMoney) {
         accounts = new double[n];
         Arrays.fill(accounts, initMoney);
-        lock = new ReentrantLock();
-        condition = lock.newCondition();
     }
 
-    public void transfer(int from, int to, double amount) {
-        lock.lock();
-        try {
-            System.out.print(Thread.currentThread().getName());
-
-            while (accounts[from] < amount) {
-                System.out.printf(" Not enough: %.2f \n", amount);
-                try {
-                    condition.await();
-                } catch (InterruptedException e){
-                    e.printStackTrace();
-                }
+    public synchronized void transfer(int from, int to, double amount) {
+        System.out.print(Thread.currentThread().getName());
+        while (accounts[from] < amount) {
+            System.out.printf(" Not enough: %.2f \n", amount);
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            accounts[from] -= amount;
-            System.out.printf(" %10.2f form %d to %d", amount, from, to);
-            accounts[to] += amount;
-            System.out.printf(" Total Balance: %10.2f\n", getTotalBank()); //10 khoảng trắng và 2 số đằng sau
-            condition.signalAll();
-        } finally {
-            lock.unlock();
         }
+        accounts[from] -= amount;
+        System.out.printf(" %10.2f form %d to %d", amount, from, to);
+        accounts[to] += amount;
+        System.out.printf(" Total Balance: %10.2f\n", getTotalBank()); //10 khoảng trắng và 2 số đằng sau
+        notifyAll();
     }
 
     public double getTotalBank() {
